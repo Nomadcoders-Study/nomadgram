2c7e507f7749db90cdd1af7827baa3b927524e9b
yykdev <assavictory@gmail.com>
2018-11-13 23:58:21 +0900

    User 모델 생성

--- django_app/db/models/users.py
+++ django_app/db/models/users.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from util import Constant
+
+
+class User(AbstractUser):
+
+    profile_image = models.ImageField(
+        null=True,
+    )
+    name = models.CharField(
+        _('Name of User'),
+        blank=True,
+        max_length=255,
+    )
+    website = models.URLField(
+        null=True,
+    )
+    bio = models.TextField(
+        null=True
+    )
+    phone = models.CharField(
+        max_length=140,
+        null=True
+    )
+    gender = models.CharField(
+        max_length=80,
+        choices=Constant.GENDER_CHOICES,
+        null=True,
+    )
+    followers = models.ManyToManyField(
+        "self",
+        blank=True,
+    )
+    following = models.ManyToManyField(
+        "self",
+        blank=True,
+    )
+    push_token = models.TextField(
+        default='',
+    )
+
+    def __str__(self):
+        return self.username
