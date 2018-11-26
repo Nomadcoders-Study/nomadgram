afb592c576fa80a0970e7477099fd77baeb1bb86
yykdev <assavictory@gmail.com>
2018-11-27 00:06:50 +0900

    LikeSerializer 클래스 구성

--- django_app/images/serializers.py
+++ django_app/images/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from db.models import Images, Comment
+from db.models import Images, Comment, Like
 
 
 class ImageSerializer(serializers.Serializer):
@@ -13,4 +13,11 @@ class CommentSerializer(serializers.Serializer):
 
     class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class LikeSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Like
         fields = '__all__'
