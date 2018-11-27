7a8325faf14d8aa7d918d9c94720886a6cb47807
yykdev <assavictory@gmail.com>
2018-11-27 14:54:22 +0900

    이미지 리스트 출력 클래스 뷰 생성

--- django_app/images/views.py
+++ django_app/images/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from db.models import Images
+from images.serializers import ImageSerializer
+
+
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+
+        all_images = Images.objects.all()
+        serializer = ImageSerializer(all_images, many=True)
+
+        return Response(data=serializer.data)
