d14086cfebf875f5857bff6b290141cc2e7a8734
yykdev <assavictory@gmail.com>
2018-11-30 16:37:16 +0900

    퍼미션 설정 임시 수정

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -76,9 +76,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
