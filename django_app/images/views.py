0a24cda1cec9690590fb8961809b1ea57322b496
yykdev <assavictory@gmail.com>
2018-12-03 18:16:13 +0900

    이미지 라이크 기능 추가

--- django_app/images/views.py
+++ django_app/images/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from db.models import Image, Comment, Like
 from images.serializers import ImageSerializer, CommentSerializer, LikeSerializer
 
@@ -34,6 +34,23 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
-        print(image_id)
+        user = request.user
 
-        return Response(status=200)
+        try:
+            image = Image.objects.get(id=image_id)
+        except Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisiting_like = Like.objects.get(
+                creator=user,
+                image=image,
+            )
+            preexisiting_like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Like.DoesNotExist:
+            new_like = Like.objects.create(
+                creator=user,
+                image=image,
+            )
+            return Response(status=status.HTTP_201_CREATED)
