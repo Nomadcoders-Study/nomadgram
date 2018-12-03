c1a8e1a33886d7c92d6c7fc123c6a61583a1188c
yykdev <assavictory@gmail.com>
2018-12-03 13:06:58 +0900

    User 모델 ManyToMany 필드 symmetrical 옵션 False로 변경

--- django_app/db/models/users.py
+++ django_app/db/models/users.py
@@ -33,13 +33,13 @@ class User(AbstractUser):
     followers = models.ManyToManyField(
         "self",
         blank=True,
-        symmetrical=True,
+        symmetrical=False,
         related_name='nomadgram_followers',
     )
     following = models.ManyToManyField(
         "self",
         blank=True,
-        symmetrical=True,
+        symmetrical=False,
         related_name='nomadgram_following',
     )
     push_token = models.TextField(
