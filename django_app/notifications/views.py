f38b1e8c5d4cf97c754c5b2f3d83e78f028d5e5c
yykdev <assavictory@gmail.com>
2018-12-05 18:12:06 +0900

    알림 생성 함수

--- django_app/notifications/views.py
+++ django_app/notifications/views.py
@@ -17,3 +17,14 @@ class Notifications(APIView):
         serializer = NotificationSerializer(notifications, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+def create_notification(creator, to, type, image=None, comment=None):
+
+    notification = Notification.objects.create(
+        creator=creator,
+        to=to,
+        notification_type=type,
+        image=image,
+        comment=comment,
+    )
