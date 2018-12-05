207752ab144519bacff5355f0ca8bb35a0f8764a
yykdev <assavictory@gmail.com>
2018-12-05 15:50:44 +0900

    유저네임을 이용한 포스팅 검색 기능

--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    url(r'^explore/$', view=views.ExploreUsers.as_view(), name='explore_user'),
-    url(r'^(?P<user_id>\d+)/follow/$', view=views.FollowUser.as_view(), name='follow_user'),
-    url(r'^(?P<user_id>\d+)/unfollow/$', view=views.UnFollowUser.as_view(), name='unfollow_user'),
-    url(r'^(?P<username>\w+)/$', view=views.UserProfile.as_view(), name='user_profile'),
-    url(r'^(?P<username>\w+)/followers/$', view=views.UserFollowers.as_view(), name='user_followers'),
-    url(r'^(?P<username>\w+)/following/$', view=views.UserFollowing.as_view(), name='user_following'),
+    url(r'^explore/$', views.ExploreUsers.as_view(), name='explore_user'),
+    url(r'^(?P<user_id>\d+)/follow/$', views.FollowUser.as_view(), name='follow_user'),
+    url(r'^(?P<user_id>\d+)/unfollow/$', views.UnFollowUser.as_view(), name='unfollow_user'),
+    url(r'^search/$', views.Search.as_view(), name='search'),
+    url(r'^(?P<username>\w+)/$', views.UserProfile.as_view(), name='user_profile'),
+    url(r'^(?P<username>\w+)/followers/$', views.UserFollowers.as_view(), name='user_followers'),
+    url(r'^(?P<username>\w+)/following/$', views.UserFollowing.as_view(), name='user_following'),
 ]
--- django_app/users/views.py
+++ django_app/users/views.py
@@ -97,3 +97,21 @@ class UserFollowing(APIView):
         serializer = ListUserSerializer(following, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class Search(APIView):
+
+    def get(self, request, format=None):
+
+        username = request.query_params.get('username', None)
+
+        if username is not None:
+
+            users = User.objects.filter(username__icontains=username)
+
+            serializer = ListUserSerializer(users, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
